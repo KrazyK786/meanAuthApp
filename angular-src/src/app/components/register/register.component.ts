5701216a30308e255a278a34ea9aa4f6b36f0437
Kristopher Johnson <krazyk786@gmail.com>
2020-03-16 21:31:54 -0500

    configured validate service and testing implementation

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ValidateService} from "../../services/validate.service";
 
 @Component({
   selector: 'app-register',
@@ -12,9 +13,31 @@ export class RegisterComponent implements OnInit {
   email: String;
   password: String;
 
-  constructor() { }
+  constructor(private validateService: ValidateService) { }
 
   ngOnInit() {
+  }
+
+  onRegisterSubmit(){
+    const user = {
+      name: this.name,
+      username: this.username,
+      email: this.email,
+      password: this.password
+    }
+
+    // Required Fields
+    if (!this.validateService.validateRegister(user)){
+      console.log('Please fill in all fields');
+      return false;
+    }
+
+    // Validate email
+    if (!this.validateService.validateEmail(user.email)){
+      console.log('Please use a valid email');
+      return false;
+    }
+
   }
 
 }
